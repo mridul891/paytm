1214b62c2d4f0ebcbe359e9efcf8a0bcdc2c065e
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-07 19:21:23 +0530

    user routes created

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { Router } = require("./routes");
-
+const mainRouter = require("./routes/index")
 
 mongoose.connect('mongodb+srv://pandeymridulwork:mridul891@paytm.gzqo0ig.mongodb.net/').then(() => console.log("mongodb connected"))
 
-app.use("/api/v1", Rout)
+app.use("/api/v1", mainRouter)
 app.listen(port, () => {
     console.log(`the app is running at  localhost :${port}`)
 })
